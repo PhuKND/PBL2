add78686b5f42dd6c7d513e3828c744e4b094fba
PhuKND <151339883+PhuKND@users.noreply.github.com>
2023-12-02 09:03:47 +0700

    Update Supplier.h

--- src/include/Supplier.h
+++ src/include/Supplier.h
@@ -9,11 +9,19 @@ private:
     Vector<Product> productsSupplied  ; 
     Vector<Order> Orders ; 
 public:
-    Supplier(/* args */);
+    Supplier();
+    Supplier(int Id, char* name,  char* address, char* phoneNumber,  char* email)
     ~Supplier();
     int getSupplierID() ; 
     void setSupplierID(int) ; 
+    char* getName() const;
+    char* getAndress() const;
+    char* getPhoneNumber() const;
+    char* getEmail() const;
     void addProduct(const Product) ; 
-     
-
-};
+    void Display() ; 
+    friend std::ostream& operator<<(ostream& o,const Supplier&supplier );
+    void TypeInformation();
+    void WriteDataToFile(std::ostream& file) const ; 
+    void ReadDataFromFile(std::istream& file)  ; 
+}; 
